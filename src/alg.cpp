2cd8088cec7820905a1b573e252e32bb2ef7eb33
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-05-30 21:15:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,23 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> TREE;
+  std::ifstream file(filename);
+	if (!file.is_open())
+		throw std::string("The file don't open!!!");
+	std::string line, w;
+  while (std::getline(file, line)) {
+    std::stringstream ss(line);
+    while (ss >> w) {
+      for (char& c : w) {
+        c = std::tolower(c); // понижение в регистре
+        if (!std::isalpha(c)) { // проверка на алфавит
+          w = w.substr(0, w.size() - 1);
+				}
+			}
+			TREE.addNode(w);
+		}
+	}
+	file.close();
+	return TREE;
 }
